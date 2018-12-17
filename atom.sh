b4504cba0b4c5a59c673ca26fb10fe8f00023b75
Unai Motriko <unaimotriko@gmail.com>
2018-12-17 10:39:55 +0000

    Create atom.sh

---
+++ atom.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Install Atom Plugins
+echo "Downloading Atom Plugins..."; echo
+apm install atom-terminal
+apm install platformio-ide-terminal
+apm install react
+apm install script
+apm install atom-typescript
+apm install language-terraform
+
+echo "Atom Plugins Download Complete"; echo
